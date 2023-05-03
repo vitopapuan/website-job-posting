be2d1d43e33791e7957c2600f8830b4625573f94
vitopapuan <vitopapuan2nd@gmail.com>
2023-05-03 14:37:47 +0700

    Revert "Added Auth"
    This reverts commit 50f0d8a313b952191a0e271bc41d24b419825e2e.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { HiBriefcase } from 'react-icons/hi'
 import Button from './Button'
 
@@ -22,9 +21,7 @@ const Hero = ({ isLoading }) => {
               quos quod deserunt assumenda sunt itaque esse veniam impedit
               facilis!
             </p>
-            <Link to='/website-job-posting/job-lists' className='py-3'>
-              <Button text={'Get Started'} />
-            </Link>
+            <Button link={'/website-job-posting/job-lists'} text={'Get Started'} />
           </div>
           <div
             className='hidden w-1/2 bg-cover bg-center rounded-xl mr-8 xl:block'
--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,15 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { auth } from '../../config/firebaseConfig'
-import { createUserWithEmailAndPassword } from 'firebase/auth'
 import { FaBriefcase } from 'react-icons/fa'
 import Loading from '../components/Loading'
 
 const LogIn = () => {
   const [isLoading, setIsLoading] = useState(false)
-  
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
 
   useEffect(() => {
     setIsLoading(true)
@@ -17,10 +12,6 @@ const LogIn = () => {
       setIsLoading(false)
     }, 1000)
   }, [])
-
-  const logIn = async () => {
-    await createUserWithEmailAndPassword(auth, email, password)
-  }
 
   return (
     <div className='flex h-screen max-w-lg flex-1 flex-col justify-center mx-auto px-6'>
@@ -50,7 +41,6 @@ const LogIn = () => {
                     type='email'
                     autoComplete='email'
                     required
-                    onChange={(e) => setEmail(e.target.value)}
                     className='block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6'
                   />
                 </div>
@@ -68,7 +58,6 @@ const LogIn = () => {
                     type='password'
                     autoComplete='current-password'
                     required
-                    onChange={(e) => setPassword(e.target.value)}
                     className='block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6'
                   />
                 </div>
@@ -77,7 +66,6 @@ const LogIn = () => {
               <div>
                 <button
                   type='submit'
-                  onClick={logIn}
                   className='flex w-full justify-center rounded-md bg-blue-600 p-2 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600'>
                   Log in
                 </button>
